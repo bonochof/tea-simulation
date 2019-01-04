e1d0cbbf726126f8a9ca5ab6cfa7ed1c3477bb72
bonochof <ryos427@yahoo.co.jp>
2019-01-05 04:37:42 +0900

    draw tea image

--- simu.rb
+++ simu.rb
@@ -4,9 +4,18 @@ t = 0
 k = 0.96
 To = 100
 Te = 28
+
+teas = [Image.load('image/tea1.png'),
+        Image.load('image/tea2.png'),
+        Image.load('image/tea3.png')]
+
 Window.loop do
   break if Input.key_push?(K_ESCAPE)
   temp = (To - Te) * k**t + Te
-  p temp
+  #p temp
   t += 1
+  
+  img = temp / To * 2
+  p img
+  Window.draw_scale(-200, 30, teas[img], 0.2, 0.2)
 end
