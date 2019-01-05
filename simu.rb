8084d2aa4f6dce2fd1d63890bcb93b13615ea221
bonochof <ryos427@yahoo.co.jp>
2019-01-05 09:45:39 +0900

    fix image id deside

--- simu.rb
+++ simu.rb
@@ -2,19 +2,12 @@ require 'dxruby'
 
 t = 0
 k = [0.96] * 5
-To = [100, 90, 80, 70, 60]
+To = [100, 90, 80, 70, 30]
 Te = [28] * 5
 
-imgd = []
-5.times do |i|
-  imgd << (To[i] - Te[i]) / 3
-end
-
-p imgd
-
-teas = [Image.load('image/tea1.png'),
-        Image.load('image/tea2.png'),
-        Image.load('image/tea3.png')]
+img = [Image.load('image/tea3.png'),
+       Image.load('image/tea2.png'),
+       Image.load('image/tea1.png')]
 pos = [-260, -140, -20, 100, 220]
 
 Window.loop do
@@ -22,9 +15,11 @@ Window.loop do
   
   5.times do |i|
     temp = (To[i] - Te[i]) * k[i]**t + Te[i]
-    img = ((temp - Te[i]) / imgd[i]).floor % 3
-    p temp, img if i == 0
-    Window.draw_scale(pos[i], 30, teas[img], 0.2, 0.2)
+    if    temp > 80 then id = 0
+    elsif temp > 40 then id = 1
+    else                 id = 2
+    end
+    Window.draw_scale(pos[i], 30, img[id], 0.2, 0.2)
   end
   
   t += 1
