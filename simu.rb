17d2318ec4ddb54bd45a4982c237a0c1888f4f6d
bonochof <ryos427@yahoo.co.jp>
2019-01-05 09:05:37 +0900

    add image id calculation

--- simu.rb
+++ simu.rb
@@ -4,6 +4,7 @@ t = 0
 k = 0.96
 To = 100
 Te = 28
+imgd = (To - Te) / 2
 
 teas = [Image.load('image/tea1.png'),
         Image.load('image/tea2.png'),
@@ -12,10 +13,9 @@ teas = [Image.load('image/tea1.png'),
 Window.loop do
   break if Input.key_push?(K_ESCAPE)
   temp = (To - Te) * k**t + Te
-  #p temp
+  p temp
   t += 1
   
-  img = temp / To * 2
-  p img
+  img = (temp / imgd).floor
   Window.draw_scale(-200, 30, teas[img], 0.2, 0.2)
 end
